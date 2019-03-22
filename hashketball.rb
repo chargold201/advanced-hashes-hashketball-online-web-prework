4a91143f8e04cc6076f9ad1852973c59186753a5
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-22 14:32:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,11 +122,73 @@ def num_points_scored(player)
     team_data.collect do |attribute, data|
       if attribute == :players
         data.collect do |name, player_data|
-          binding.pry
-          if name == player
+          if name.to_s == player
             player_data.collect do |player_attribute, data|
               if player_attribute == :points
                 return data
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+def shoe_size(player)
+  game_hash.collect do |location, team_data|
+    team_data.collect do |attribute, data|
+      if attribute == :players
+        data.collect do |name, player_data|
+          if name.to_s == player
+            player_data.collect do |player_attribute, data|
+              if player_attribute == :shoe
+                return data
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  game_hash.collect do |location, team_data|
+    team_data.collect do |attribute, data|
+      if data == team
+        return game_hash[location][:colors]
+      end
+    end
+  end
+end
+
+def team_names
+  array = []
+  game_hash.collect do |location, team_data|
+    team_data.collect do |attribute, data|
+      if attribute == :team_name
+        array << data
+      end
+    end
+  end
+  array
+end
+
+def player_numbers(team)
+  
+end
+
+def big_shoe_rebounds
+  biggest_shoe = 0
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, player_data|
+          player_data.each do |player_attribute, personal_data|
+            if player_attribute == :shoe
+              if personal_data > biggest_shoe
+                biggest_shoe = personal_data
               end
             end
           end
